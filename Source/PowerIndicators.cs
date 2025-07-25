4ed4889b327f847cf1673e0d376b02e5018c7b64
Bert <7046024+Pentasis@users.noreply.github.com>
2025-07-25 15:39:48 +0200

    Refactor structure

--- Source/PowerIndicators.cs
+++ Source/PowerIndicators.cs
@@ -10,17 +10,28 @@ namespace ConsolidatedMods.Textures
     [StaticConstructorOnStartup]
     internal class PowerIndicators
     {
+        private const float IconSize = 32f / 3 * 2;
+        private const float PowerIconOffset = 32f / 3 * 2 / 4 * 3;
+        private const float FuelIconOffset = 32f / 4 * 3;
+
         private static readonly Texture2D powerIcon = ContentFinder<Texture2D>.Get(itemPath: "UI/Overlays/NeedsPower");
 
         private static readonly Dictionary<string, Texture2D> fuelIcon = new Dictionary<string, Texture2D>();
 
+        private static Graphic[] fireSubGraphics;
         static PowerIndicators()
+
         {
             Harmony harmony = new Harmony(id: "consolidatedmods.textures.powerindicators");
             harmony.Patch(original: AccessTools.Method(type: AccessTools.Method(type: typeof(Designator_Build), name: nameof(Designator_Build.GizmoOnGUI)).DeclaringType, name: nameof(Designator_Build.GizmoOnGUI)),
                                                         postfix: new HarmonyMethod(methodType: typeof(PowerIndicators), methodName: nameof(DrawIndicator)));
             harmony.Patch(original: AccessTools.Method(type: AccessTools.Method(type: typeof(Designator_Dropdown), name: nameof(Designator_Dropdown.GizmoOnGUI)).DeclaringType, name: nameof(Designator_Dropdown.GizmoOnGUI)),
                           postfix: new HarmonyMethod(methodType: typeof(PowerIndicators), methodName: nameof(DrawIndicator)));
+
+            if (ThingDefOf.Fire.graphic is Graphic_Collection graphicCollection)
+                fireSubGraphics = Traverse.Create(graphicCollection).Field("subGraphics").GetValue<Graphic[]>();
+            else
+                fireSubGraphics = new Graphic[] { ThingDefOf.Fire.graphic };
         }
 
         public static void DrawIndicator(Command __instance, Vector2 topLeft)
@@ -36,21 +47,33 @@ namespace ConsolidatedMods.Textures
 
             if (currentThingDef.ConnectToPower)
             {
-                GUI.DrawTexture(new Rect(x: topLeft.x + __instance.GetWidth(maxWidth: float.MaxValue) - 32f / 3 * 2 / 4 * 3, y: topLeft.y, width: 32f / 3 * 2, height: 32f / 3 * 2), image: powerIcon, ScaleMode.ScaleToFit);
+                GUI.DrawTexture(new Rect(x: topLeft.x + __instance.GetWidth(maxWidth: float.MaxValue) - PowerIconOffset, y: topLeft.y, width: IconSize, height: IconSize), image: powerIcon, ScaleMode.ScaleToFit);
 
             }
             else if (currentThingDef.GetCompProperties<CompProperties_Refuelable>() is CompProperties_Refuelable refuelProperties && refuelProperties.fuelFilter.AllowedThingDefs.Any())
             {
                 ThingDef fuelDef = refuelProperties.fuelFilter.AllowedThingDefs.First();
-                Graphic fuelGraphic = fuelDef.graphic;
-                string iconPath = fuelGraphic is Graphic_Collection graphicCollection ? Traverse.Create(root: graphicCollection).Field(name: "subGraphics").GetValue<Graphic[]>().Last().path : fuelGraphic.path;
-                if (iconPath.NullOrEmpty())
-                    iconPath = fuelDef.graphicData != null ? fuelDef.graphicData.texPath : Traverse.Create(root: ThingDefOf.Fire.graphic).Field(name: "subGraphics").GetValue<Graphic[]>().RandomElement().path;
+                string iconPath = GetFuelIconPath(fuelDef);
+
                 if (!fuelIcon.ContainsKey(key: iconPath))
                     fuelIcon.Add(key: iconPath, value: ContentFinder<Texture2D>.Get(itemPath: iconPath));
 
-                GUI.DrawTexture(new Rect(x: topLeft.x + __instance.GetWidth(maxWidth: float.MaxValue) - 32f / 4 * 3, y: topLeft.y, width: 32f / 3 * 2, height: 32f / 3 * 2), image: fuelIcon[iconPath], ScaleMode.ScaleToFit);
+                GUI.DrawTexture(new Rect(x: topLeft.x + __instance.GetWidth(maxWidth: float.MaxValue) - FuelIconOffset, y: topLeft.y, width: IconSize, height: IconSize), image: fuelIcon[iconPath], ScaleMode.ScaleToFit);
             }
         }
+
+        private static string GetFuelIconPath(ThingDef fuelDef)
+        {
+            Graphic fuelGraphic = fuelDef.graphic;
+
+            if (fuelGraphic is Graphic_Collection graphicCollection)
+                return Traverse.Create(root: graphicCollection).Field(name: "subGraphics").GetValue<Graphic[]>().Last().path;
+
+            if (!fuelGraphic.path.NullOrEmpty())
+                return fuelGraphic.path;
+
+            return fuelDef.graphicData?.texPath ?? fireSubGraphics.RandomElement().path;
+        }
     }
+}
 }
